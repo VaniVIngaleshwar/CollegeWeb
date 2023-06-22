92a270a5b171c57a209ef5b5ed89b5683ba8a46c
Kartik <kchosamani.2001@gmail.com>
2023-06-22 23:01:37 +0530

    header&footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'timetable', component:TimetableComponent},
+  // keep this below line at last only dont disturb it ur remainig path u can attach above ok
+  {path:'', redirectTo:'home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
